5981d515b517d314975419040f4804b7f5e85487
scavenOP <74195732+scavenOP@users.noreply.github.com>
2020-11-10 09:54:26 +0530

    changed

--- bot.py
+++ bot.py
@@ -18,9 +18,13 @@ async def on_ready():
 
 
 @bot.command()
-async def hi(ctx):
-    await ctx.send('hello!')
-    await ctx.message.delete()
+async def help(ctx):
+    embed=discord.Embed(
+        description='**CLEAR MESSAGES**\n```,clear <no of messeges>```\n**EMBED Messeges**\n```,say <massege>```\n**KICK MEMBER**\n```,kick <member> <reason>```\n**BAN MEMBER**\n```,ban <member> <reason>```\n**UNBAN MEMBER**\n```,unban <member>```\n**WARN MEMBER**\n```,warn <member> <reason>```\n**SHOW AVATAR**\n```,avatar <member> ```',
+        color = discord.Color.blue()
+    )
+    
+    await ctx.send(embed=embed) 
 
 @bot.command()
 @commands.has_permissions(manage_messages=True)
