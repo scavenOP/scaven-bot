63a24e154ebca652753dd4adbd8700789615cdbc
Niladri Samanta <74195732+scavenOP@users.noreply.github.com>
2023-12-14 22:08:18 +0530

    Update int.py

--- int.py
+++ int.py
@@ -1,22 +1,2 @@
-Exception in thread "main" com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException: Unrecognized field "CountryDialingCode" (class Models.ContactProvided), not marked as ignorable (3 known properties: "EmailAddressValue", "Label", "Phone"])
- at [Source: (File); line: 142, column: 62] (through reference chain: Models.OrderCreateRQ["Query"]->Models.Query["DataLists"]->Models.DataLists["ContactList"]->java.util.ArrayList[0]->Models.ContactInformation["ContactProvided"]->java.util.ArrayList[0]->Models.ContactProvided["CountryDialingCode"])
-
-
-public class ContactInformation {
-    public String ContactID;
-    public String ContactType;
-    public List<ContactProvided> ContactProvided;
-
-}
-class ContactProvided{
-    public String EmailAddressValue;
-    public Phone Phone;
-    public String Label;
-}
-class Phone{
-    public String Label;
-    public String CountryDialingCode;
-    public String AreaCode;
-    public String PhoneNumber;
-
-}
+Exception in thread "main" com.fasterxml.jackson.databind.exc.MismatchedInputException: Cannot construct instance of `Models.OfferItem` (although at least one Creator exists): no String-argument constructor/factory method to deserialize from String value ('SH1')
+ at [Source: (File); line: 28, column: 33] (through reference chain: Models.OrderCreateRQ["Query"]->Models.Query["Order"]->Models.Order["Offer"]->Models.Offer["OfferItem"]->java.util.ArrayList[0])
